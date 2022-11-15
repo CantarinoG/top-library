ba5ef492d9233bbafe68a8ebc830a6ec2cecd097
CantarinoG <guilhermecantarino1709@gmail.com>
2022-11-15 14:51:49 -0300

    Refactored code using class syntax

--- scripts/library.js
+++ scripts/library.js
@@ -1,22 +1,41 @@
-let bookList = [];
+let bookList = []; //Contains all the books
 
 let currentBook = null;
 
-const form = document.querySelector("#add > div.offcanvas-body > form");
-const titleField = document.querySelector("#title");
-const authorField = document.querySelector("#author");
-const pagesField = document.querySelector("#pages");
-const readField = document.querySelector("#add > div.offcanvas-body > form > div.form-check.mb-3 > label > input");
-const tBody = document.querySelector("body > main > div > table > tbody");
+const form = document.querySelector("#add > div.offcanvas-body > form"); //Form element
+const titleField = document.querySelector("#title"); //Title field in the form element
+const authorField = document.querySelector("#author"); //Author field in the form element
+const pagesField = document.querySelector("#pages"); //Pages field in the form element
+const readField = document.querySelector("#add > div.offcanvas-body > form > div.form-check.mb-3 > label > input"); //Checkbox field in the form element
+const tBody = document.querySelector("body > main > div > table > tbody"); //Table body where the list of books is rendered
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book { //Book class
+
+    constructor(title, author, pages, read) {
+        this._title = title;
+        this._author = author;
+        this._pages = pages;
+        this._read = read;
+    }
+
+    get title() {
+        return this._title;
+    }
+
+    get author() {
+        return this._author;
+    }
+
+    get pages() {
+        return this._pages;
+    }
+
+    get read() {
+        return this._read;
+    }
 }
 
-function getInfoFromForm() {
+function getInfoFromForm() { //Get info from the fields and generate a new book(the currentBook)
     let title = titleField.value;
     let author = authorField.value;
     let pages = pagesField.value;
@@ -24,19 +43,19 @@ function getInfoFromForm() {
     currentBook = new Book(title, author, pages, read);
 }
 
-function addBookToLibrary() {
+function addBookToLibrary() { //Add a book to the bookList
     getInfoFromForm();
     bookList.push(currentBook);
 }
 
-function clearForm() {
+function clearForm() { //Clear the form
     titleField.value = "";
     authorField.value = "";
     pagesField.value = "";
     readField.checked = false;
 }
 
-function renderTable() {
+function renderTable() { //Render the table
     tBody.innerHTML = "";
     for (let i = 0; i < bookList.length; i++) {
         let book = bookList[i];
@@ -63,7 +82,7 @@ function renderTable() {
 
 }
 
-function deleteBook(title) {
+function deleteBook(title) { //delete book: the book with the title will be removed from the list and the table will be rendered again
     for (let i = 0; i < bookList.length; i++) {
         if (bookList[i].title === title) {
             bookList.splice(i, 1);
@@ -73,7 +92,7 @@ function deleteBook(title) {
     renderTable();
 }
 
-form.addEventListener("submit", (e) => {
+form.addEventListener("submit", (e) => { //Change what happens when the form is submitted: Add book to the list, clear the form and render the table
     e.preventDefault();
     addBookToLibrary();
     clearForm();
